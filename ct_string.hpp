7212648d756645c1059c2e19e9e7010dbfbb304b
tim42 <timothee.feuillet@gmail.com>
2016-02-02 18:00:18 +0100

    [CT/STRING]: Fix an issue with g++ 5.2 than can't build yaggler because of the pointer check in ct::strlen

--- ct_string.hpp
+++ ct_string.hpp
@@ -49,84 +49,15 @@ namespace neam
     // strlen function
     inline static constexpr size_t strlen(const char *const str)
     {
-      return str ? internal::strlen(str) : 0;
+//       return str ? internal::strlen(str) : 0; // GCC 5.2.1 HATE this on constexpr string_t...
+      return internal::strlen(str);
     }
-/*
-    // Compile time string class
-    // support substring, comparison,
-    // casting to char (Except when it's a substring, and when the end is < the length of the string)
-    // length computation,
-    template <const char *Str, size_t Start = 0, size_t End = npos>
-    class string
+
+    // a safe strlen function
+    inline static constexpr size_t safe_strlen(const char *const str)
     {
-      private: // helpers
-        // comparison helpers
-        template<const char *const S1, const char *const S2, size_t Pos = Start>
-        inline static constexpr typename std::enable_if < S1[Pos] != S2[Pos], bool >::type str_is_same()
-        {
-          return false;
-        }
-        template<const char *const S1, const char *const S2, size_t Pos = Start>
-        inline static constexpr typename std::enable_if < S1[Pos] == S2[Pos] && (S1[Pos] == 0 || S2[Pos] == 0 || Pos == End), bool >::type str_is_same()
-        {
-          return true;
-        }
-        template<const char *const S1, const char *const S2, size_t Pos = Start>
-        inline static constexpr typename std::enable_if < S1[Pos] == S2[Pos] && S1[Pos] != 0 && S2[Pos] != 0 && Pos != End, bool >::type str_is_same()
-        {
-          return str_is_same < S1, S2, Pos + 1 > ();
-        }
-
-      public:
-        constexpr string() {}
-        ~string() {}
-
-        // allow casting back the class to a const char *
-        constexpr operator const char *() const
-        {
-          return value + Start;
-        }
-
-        // comparison operators
-        template<const char *OStr>
-        constexpr bool operator == (const string<OStr> &) const
-        {
-          return str_is_same<Str, OStr>();
-        }
-        template<const char *OStr>
-        constexpr bool operator != (const string<OStr> &other) const
-        {
-          return !(*this == other);
-        }
-
-        constexpr char operator[](size_t pos) const
-        {
-          return pos + Start < End ? Str[pos + Start] : 0;
-        }
-
-      private: // helpers
-        // length computation
-        template<size_t Pos>
-        constexpr static typename std::enable_if<Str[Pos], size_t>::type _compute_length()
-        {
-          return _compute_length < Pos + 1 > () + 1;
-        }
-        template<size_t Pos>
-        constexpr static typename std::enable_if < !Str[Pos], size_t >::type _compute_length()
-        {
-          return 0;
-        }
-        constexpr static size_t compute_length()
-        {
-          return _compute_length<0>();
-        }
-
-      public:
-        static constexpr const char *value = Str + Start; // data
-        static constexpr size_t length = compute_length(); // length of the string
-        static constexpr size_t start_index = Start;
-        static constexpr size_t end_index = End;
-    };*/
+      return str ? internal::strlen(str) : 0; // GCC 5.2.1 HATE this on constexpr string_t...
+    }
   } // namespace ct
 } // namespace neam
 
